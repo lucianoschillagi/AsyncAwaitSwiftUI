d55a3593007a77eed702c065ded83137390ff2de
luko <luko.web@gmail.com>
2022-07-28 18:33:50 -0300

    Initial Commit

---
+++ AsyncAwaitSwiftUI/AsyncAwaitSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AsyncAwaitSwiftUIApp.swift
+//  AsyncAwaitSwiftUI
+//
+//  Created by Luko on 28/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct AsyncAwaitSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
